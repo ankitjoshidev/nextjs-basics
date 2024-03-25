e7cd6467033d4685e5ecf5d21af75e27815a7f4b
Ankit Joshi <ankitjoshi66259@gmail.com>
2024-03-25 21:17:18 +0530

    changes in mongoose connection

--- src/components/lib/utils.js
+++ src/components/lib/utils.js
@@ -8,7 +8,7 @@ export const connectToDb = async () => {
       console.log("Using existing connection");
       return;
     }
-    const db = await mongoose.connect("mongodb+srv://ankitjoshi66259:<password>@cluster0.3au20ye.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    const db = await mongoose.connect(process.env.MONGO);
     connection.isConnected = db.connections[0].readyState;
   } catch (error) {
     console.log(error);
